0a1b48ce655fad63a75185255798f022955c8811
Vassilis G. Kotsmanidis <vassilis.kotsman@gmail.com>
2021-09-22 03:34:21 +0300

    commit no1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts = [
+    {
+      title: "Hanged Man's Tree",
+      imageUrl: "assets/images/hanged-mans-tree.jpg",
+      username: 'Geralt',
+      content: "Many bodies hanging from there, a real feast for the crows."
+    },
+    {
+      title: "Gondolin",
+      imageUrl: "assets/images/gondolin.jpg",
+      username: 'Turin',
+      content: "Nice city, the Dark Lord will never find it."
+    },
+    {
+      title: "Morgoth curses Turin",
+      imageUrl: "assets/images/morgoth-punishes-hurin.jpg",
+      username: 'Morgoth',
+      content: "Sit now there; and look out upon the lands where evil and despair shall come upon those whom you lovest. Thou hast dared to mock me, and to question the power of Melkor, Master of the fates of Arda. Therefore with my eyes thou shalt see, and with my ears shalt hear; and never shalt thou move from this place until all is fulfilled unto its bitter end."
+    }
+  ]
 }
